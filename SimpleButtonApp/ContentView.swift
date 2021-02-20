43e214383fa87faf8a2ac7ff1204b21019e1bc4c
chefeleUNH <chefele@newhaven.edu>
2021-02-20 11:15:15 -0500

    created layout for a simple button app

--- SimpleButtonApp/ContentView.swift
+++ SimpleButtonApp/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 15) {
+            Text("Mark's App")
+                .font(.largeTitle)
+            
+            Link("Launch webcast",
+                 destination: URL(string: "https://www.zoom.com")!)
+                .font(.largeTitle)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(height: 75)
+                .foregroundColor(.black)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint:  .leading, endPoint: .trailing))
+                .cornerRadius(5)
+            
+            Link("Launch website",
+                 destination: URL(string: "https://www.newhaven.edu")!)
+                .font(.largeTitle)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(height: 75)
+                .foregroundColor(.black)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint:  .leading, endPoint: .trailing))
+                .cornerRadius(5)
+        }
+        .padding(50)
     }
 }
 
